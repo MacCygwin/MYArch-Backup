7224be5b16c256a1d09e072962fcad814a328a22
MacCygwin <mdyusufpool@gmail.com>
2024-06-04 18:39:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland alacritty wofi waybar dolphin qt5 qt5ct qt6ct grim slurp nwg-look lxappearance hyprpaper brightnessctl pavucontrol fish power-profiles-daemon thunar ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji gtk-engine-murrine gtk-engines blueman bluez bluez-utils hyprlock hypridle wireplumber pipewire pulseaudio gvfs ttf-jetbrains-mono-nerd noto-fonts-emoji polkit-kde-agent xdg-desktop-portal-hyprland mako
-yay -S wlogout
+sudo pacman -S hyprland alacritty wofi waybar dolphin qt5 qt5ct qt6ct grim slurp nwg-look lxappearance hyprpaper brightnessctl pavucontrol fish power-profiles-daemon thunar ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji gtk-engine-murrine gtk-engines blueman bluez bluez-utils hyprlock hypridle wireplumber pipewire pipewire-alsa alsa-utils pipewire-pulse gvfs ttf-jetbrains-mono-nerd noto-fonts-emoji polkit-kde-agent xdg-desktop-portal-hyprland mako
+yay -S wlogout catppuccin-cursors-macchiato
